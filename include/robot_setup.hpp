b5c962dc440a0dc5033364f33824727dc2aed1c0
jasperonvex <thejames005@outlook.com>
2026-03-20 10:08:34 -0400

    added port thing (might need to move the initlization to a cpp)

---
+++ include/robot_setup.hpp
@@ -0,0 +1,23 @@
+#include"main.h"
+
+#pragma
+//change these as needed 
+
+//drivetrain
+extern pros::MotorGroup rightMg({1,2,3});
+extern pros::MotorGroup leftMg({4,5,6});
+
+//odomertry
+extern pros::Rotation verticalWhel(7);
+extern pros::Rotation horosontleWhel(8);
+extern pros::Imu inert(9);
+
+//distence tracking
+
+extern pros::Distance Fl(10);
+extern pros::Distance Fr(11);
+extern pros::Distance R(12);
+extern pros::Distance L(13);
+
+//add anything else down here
+
