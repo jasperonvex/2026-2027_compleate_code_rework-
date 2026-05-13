5b73790b60d56e095cbaf6d9c8200979f9d1ee53
jasperonvex <thejames005@outlook.com>
2026-05-13 10:54:14 -0400

    added commetss

--- src/mainFunctionsC/functions.cpp
+++ src/mainFunctionsC/functions.cpp
@@ -2,15 +2,17 @@
 
 
 functions function;
-
+//degrees to radians
 double functions::DegToRad(double D){
     return D * (M_PI / 180);
 }
 
+//radians to degrees
 double functions::RadToDeg(double R){
     return R * (180 / M_PI);
 }
 
+//normalizing a radian angle between pi and -pi
 double functions::normalizeAngle(double R){
     double angle = R;
     while(angle > M_PI) { angle -= 2 * M_PI;}
@@ -18,6 +20,7 @@ double functions::normalizeAngle(double R){
     return  angle;
 }
 
+//normalizing a degree angle between 180 to -180
 double functions::normalizeDegAngle(double A){
     double angle = A;
     while(angle > 180) { angle -= 360;}
@@ -25,6 +28,7 @@ double functions::normalizeDegAngle(double A){
     return  angle;
 }
 
+//quick rounding to the nearist thousond.
 double functions::roundNearistThous(double Num){
     double n = Num;
     n = round(n*1000);
@@ -32,10 +36,12 @@ double functions::roundNearistThous(double Num){
     return n;
 }
 
+// milameters to inches
 double functions::MM_to_IN(double MM){
     return MM/25.4;
 }
 
+// the distence formula.
 double functions::GetDistence(double x1, double y1, double x2, double y2){
     return sqrt(pow(x1-x2,2) + pow(y1-y2,2));
 }
