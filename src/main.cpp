e9663c680c558dedade03b06ca3e90cfc3824dd5
jasperonvex <thejames005@outlook.com>
2026-06-23 17:57:49 -0400

    done ish needs work

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,6 @@
 #include "main.h"
 #include "robot_setup.hpp"
+#include "mainFunctionsH/functions.hpp"
 
 pros::AIVision vis(5);
 
@@ -37,14 +38,16 @@ void opcontrol() {
 	double imagecenterx = image_width/2;
 
 	
-	double horosontle_fov_rad = 74 * M_PI/180;
+	double horosontle_fov_rad = 74 * (M_PI/180);
 
 	double  imageheight = 240;
 	double imagecentery = imageheight/2;
 	
 	double vertical_fov_rad = 63*(M_PI / 180);
 
+    double focal_lenght_pixles = 157.25;
 
+    Position tag[] = {{0,0},{-48,24}};
 
     while (true) {
         auto objects = vis.get_all_objects();
@@ -83,19 +86,41 @@ void opcontrol() {
 
 			double verticalbearijng = atan(pixleoffsety/fy);
 
+           // double leftWidth = function.GetDistence(object.object.tag.x0,object.object.tag.y0,object.object.tag.x1,object.object.tag.y1);
+            //double bottemWidth = function.GetDistence(object.object.tag.x3,object.object.tag.y3,object.object.tag.x2,object.object.tag.y2);
+
+           // double pixleWidht = (topWidth + bottemWidth)/2;
+
+           double rightHieght = function.GetDistence(object.object.tag.x0,object.object.tag.y0,object.object.tag.x3,object.object.tag.y3);
+           double leftHiehgt = function.GetDistence(object.object.tag.x1,object.object.tag.y1,object.object.tag.x2,object.object.tag.y2);
+           
+           double picleHieght = (rightHieght + leftHiehgt)/2;
+
+            double distence = (1 * focal_lenght_pixles)/picleHieght;
+
+            double groundDis = distence * cos(verticalbearijng);
+
+            double x_position = tag[object.id].x - (distence * cos(horosontle_bearing));
+            double y_position = tag[object.id].y - (distence * sin(horosontle_bearing));
+
+            
+
             pros::lcd::set_text(1, "id: " + std::to_string(object.id));
             pros::lcd::set_text(2, "x0: " + std::to_string(object.object.tag.x0) +
-                                   " y0: " + std::to_string(object.object.tag.y0) +
-    								"x1: " + std::to_string(object.object.tag.x1) +
-                                   " y1: " + std::to_string(object.object.tag.y1));
+                                   " || y0: " + std::to_string(object.object.tag.y0) +
+    								" || x1: " + std::to_string(object.object.tag.x1) +
+                                   " || y1: " + std::to_string(object.object.tag.y1));
             pros::lcd::set_text(3, "x2: " + std::to_string(object.object.tag.x2) +
-                                   " y2: " + std::to_string(object.object.tag.y2) +
-           							"x3: " + std::to_string(object.object.tag.x3) +
-                                   " y3: " + std::to_string(object.object.tag.y3));
+                                   " || y2: " + std::to_string(object.object.tag.y2) +
+           							" || x3: " + std::to_string(object.object.tag.x3) +
+                                   " || y3: " + std::to_string(object.object.tag.y3));
             pros::lcd::set_text(4, "cx: " + std::to_string(cx) +
-                                   " cy: " + std::to_string(cy));
-			pros::lcd::set_text(5, "horsontledeg: " + std::to_string(horosontle_bearing * (180/M_PI)));
-			pros::lcd::set_text(6, " verticalbearing: " + std::to_string(verticalbearijng * (180 * M_PI)));
+                                   " || cy: " + std::to_string(cy));
+			//pros::lcd::set_text(5, "horsontledeg: " + std::to_string(horosontle_bearing * (180/M_PI)));
+			//pros::lcd::set_text(6, " verticalbearing: " + std::to_string(verticalbearijng * (180 / M_PI)));
+            //pros::lcd::set_text(6,"tagheight: " + std::to_string(picleHieght));
+            pros::lcd::set_text(6,"x: " + std::to_string(x_position) + " || y: " +std::to_string(y_position));
+            pros::lcd::set_text(7,"distence: " + std::to_string(groundDis));
         }
 
 		
