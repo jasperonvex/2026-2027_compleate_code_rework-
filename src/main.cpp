7d1fb87e012febaeeb33903dec1fc0245d47d0bc
jasperonvex <thejames005@outlook.com>
2026-06-23 15:33:17 -0400

    testing the new ai sensor!!!!!

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,13 @@
 #include "main.h"
 #include "robot_setup.hpp"
 
+pros::AIVision vis(5);
 
 void initialize() {
 	pros::lcd::initialize();
 
+	
+	
 	
 }
 
@@ -18,12 +21,85 @@ void competition_initialize() {}
 
 void autonomous() {}
 
+struct Position {
+    double x;
+    double y;
+};
 
 void opcontrol() {
+    vis.reset();
+    pros::delay(500);
+
+    vis.enable_detection_types(pros::AivisionModeType::tags);
+    vis.set_tag_family(pros::AivisionTagFamily::tag_21H7, true);
+
+	double image_width = 320;
+	double imagecenterx = image_width/2;
+
+	
+	double horosontle_fov_rad = 74 * M_PI/180;
+
+	double  imageheight = 240;
+	double imagecentery = imageheight/2;
+	
+	double vertical_fov_rad = 63*(M_PI / 180);
 
 
-	while (true) {
+
+    while (true) {
+        auto objects = vis.get_all_objects();
+
+        if (objects.empty()) {
+            pros::lcd::set_text(1, "No tag detected");
+            pros::lcd::clear_line(2);
+            pros::lcd::clear_line(3);
+            pros::lcd::clear_line(4);
+            pros::lcd::clear_line(5);
+            pros::lcd::clear_line(6);
+            pros::lcd::clear_line(7);
+        } else {
+            auto object = objects[0];
+            double cx = (
+                object.object.tag.x0 +
+                object.object.tag.x1 +
+                object.object.tag.x2 +
+                object.object.tag.x3
+            ) / 4.0;
+
+            double cy = (
+                object.object.tag.y0 +
+                object.object.tag.y1 +
+                object.object.tag.y2 +
+                object.object.tag.y3
+            ) / 4.0;
+
+			double fx = imagecenterx / tan(horosontle_fov_rad/2.0);
+			double pixleoffset = cx - imagecenterx;
+
+			double horosontle_bearing = atan(pixleoffset/fx);
+
+			double fy = imagecentery/ tan(vertical_fov_rad/2);
+			double pixleoffsety = imagecentery - cy;
+
+			double verticalbearijng = atan(pixleoffsety/fy);
+
+            pros::lcd::set_text(1, "id: " + std::to_string(object.id));
+            pros::lcd::set_text(2, "x0: " + std::to_string(object.object.tag.x0) +
+                                   " y0: " + std::to_string(object.object.tag.y0) +
+    								"x1: " + std::to_string(object.object.tag.x1) +
+                                   " y1: " + std::to_string(object.object.tag.y1));
+            pros::lcd::set_text(3, "x2: " + std::to_string(object.object.tag.x2) +
+                                   " y2: " + std::to_string(object.object.tag.y2) +
+           							"x3: " + std::to_string(object.object.tag.x3) +
+                                   " y3: " + std::to_string(object.object.tag.y3));
+            pros::lcd::set_text(4, "cx: " + std::to_string(cx) +
+                                   " cy: " + std::to_string(cy));
+			pros::lcd::set_text(5, "horsontledeg: " + std::to_string(horosontle_bearing * (180/M_PI)));
+			pros::lcd::set_text(6, " verticalbearing: " + std::to_string(verticalbearijng * (180 * M_PI)));
+        }
+
 		
-		pros::delay(20);                               
-	}
+
+        pros::delay(100);
+    }
 }
